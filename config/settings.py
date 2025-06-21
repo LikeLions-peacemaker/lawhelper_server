5e1db6edb79497ccaa2630ac70c0a3f9d2884c17
codedsaf45 <spacetime2002@gmail.com>
2025-06-22 07:07:24 +0900

    v2

--- config/settings.py
+++ config/settings.py
@@ -86,25 +86,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'lawhelper_db',
-#         'USER': 'lawhelper_user',
-#         'PASSWORD': '1234asdf',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#             'charset': 'utf8mb4',
-#         },
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'lawhelper_db',
+        'USER': 'lawhelper_user',
+        'PASSWORD': 'asdf1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 # Password validation
